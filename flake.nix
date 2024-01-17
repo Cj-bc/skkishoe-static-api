e1ce0260aa1d94de5fb85c8d3c06e7e99e267f60
Cj-bc <cj.bc-sd@outlook.jp>
2024-01-17 22:03:40 +0900

    Add: flake.nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Static API for skkishoe";
+
+  outputs = { self, nixpkgs }:
+  let system = "x86_64-linux";
+  pkgs = nixpkgs.legacyPackages.${system};
+  in {
+
+    packages.${system} = {
+        skkishoe-static-api = pkgs.stdenv.mkDerivation {
+        	name = "skkishoe-static-api";
+	        src = pkgs.fetchFromGitHub {
+    	        owner = "Cj-bc";
+        	    repo = "skkishoe-static-api";
+        	    rev = "25da7d1b6d7b33901d03ff812749759b209217f4";
+        	    hash = "sha256-IBu3nzUrXOcBi8TSkthNxHUAycKETMn+9rRiKAHfQzE=";
+	        };
+    	    nativeBuildInputs = [pkgs.nkf];
+        };
+    	default = self.packages.x86_64-linux.skkishoe-static-api;
+    };
+  };
+}
