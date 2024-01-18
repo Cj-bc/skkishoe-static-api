c4598aadceb3256d45c076cb3f09f9ab3930cd8d
Cj-bc <cj.bc-sd@outlook.jp>
2024-01-18 11:54:47 +0900

    Fix: flake,Makefile> Specify JISYO file as another derivation
    Because we can't access internet in 'buildPhase'

--- Makefile
+++ Makefile
@@ -7,5 +7,4 @@ build: convert.awk dict
 	./convert.awk -v dst="$(DST)/midashi/" $(foreach d,$(DICTIONARIES),$(d).utf-8)
 
 dict:
-	git clone --depth=1 https://github.com/skk-dev/dict
 	for f in $(DICTIONARIES); do nkf -w8 $f > ${f}.utf-8; done
--- flake.nix
+++ flake.nix
@@ -7,6 +7,16 @@
   in {
 
     packages.${system} = {
+        skk-dict = pkgs.stdenv.mkDerivation {
+            name = "skk-JISYO.L";
+            src = pkgs.fetchzip {
+                url = "https://skk-dev.github.io/dict/SKK-JISYO.L.gz";
+                hash = "sha256-GqJ3stBaDONzHGtE3l4ixITQcRXY8MTYuM2a6QevRhM=";
+            };
+            buildPhase = ''
+            mv skk-JISYO.L $out
+            ''
+        };
         skkishoe-static-api = pkgs.stdenv.mkDerivation {
         	name = "skkishoe-static-api";
 	        src = pkgs.fetchFromGitHub {
@@ -15,7 +25,7 @@
         	    rev = "25da7d1b6d7b33901d03ff812749759b209217f4";
         	    hash = "sha256-IBu3nzUrXOcBi8TSkthNxHUAycKETMn+9rRiKAHfQzE=";
 	        };
-    	    nativeBuildInputs = [pkgs.nkf pkgs.git];
+    	    nativeBuildInputs = [pkgs.nkf pkgs.git self.packages.${system}.skk-dict];
         };
     	default = self.packages.x86_64-linux.skkishoe-static-api;
     };
